c19bd8ddbdc190c13cc7f1f1e781421d9e413bcb
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-01 01:28:50 +0700

    fix: update data products

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -4,7 +4,7 @@ export const dataProducts = [
     title: "One Piece Vol. 90",
     author: "Eiichiro Oda",
     price: 99000,
-    originalPrice: 289000,
+    originalPrice: 189000,
     category: "Comics",
     description:
       "The Straw Hat Pirates continue their adventures in the Wano Country arc, facing off against the powerful Beast Pirates and their leader, Kaido.",
@@ -13,9 +13,24 @@ export const dataProducts = [
     rating: 4.8,
     inStock: true,
     publishYear: 2021,
-    chapters: 10,
-    volumes: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    chapters: 909 - 1060,
+    volumes: 90 - 104,
+  },
+  {
+    slug: "one-piece-vol-82",
+    title: "One Piece Vol. 82",
+    author: "Eiichiro Oda",
+    price: 65900,
+    originalPrice: 89900,
+    category: "Comics",
+    description:
+      "Whole Cake Island, in this arc, Luffy sneaks onto Big Mom's island to rescue Sanji.",
+    imageUrl:
+      "https://3mqxc38j34.ucarecd.net/96e4881b-4d68-4857-9232-ce31ed7b191c/-/preview/579x900/",
+    rating: 4.7,
+    inStock: true,
+    publishYear: 2017,
+    chapters: 825 - 902,
+    volumes: 82 - 90,
   },
 ];
