e3d6bf51e4a665c36c9ab969a5feae063a39ebce
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-01 01:53:24 +0700

    feat: add Prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { prisma } from "./lib/prisma";
 
 const app = new Hono();
 
@@ -6,6 +7,11 @@ app.get("/", (c) => {
   return c.json({
     message: "Arcbook Backend REST API!",
   });
+});
+
+app.get("/products", async (c) => {
+  const products = await prisma.product.findMany();
+  return c.json(products);
 });
 
 export default app;
