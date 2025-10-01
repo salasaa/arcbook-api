dc26ba9a4ad43f4a34875948758f0fe3d0906376
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-01 15:02:06 +0700

    feat: update Product model to use categorySlug and seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,15 +4,35 @@ const prisma = new PrismaClient();
 
 import { dataProducts } from "./data/products";
 
-async function main() {
-  for (const product of dataProducts) {
-    const newProductResult = await prisma.product.upsert({
-      where: { slug: product.slug },
-      update: product,
-      create: product,
+import { dataCategories } from "./data/categories";
+
+async function seedCategories() {
+  await prisma.category.createMany({
+    data: dataCategories.map((x) => ({
+      name: x.name,
+      slug: x.slug.toLowerCase(),
+    })),
+    skipDuplicates: true,
+  });
+}
+
+async function seedProducts() {
+  for (const dataProduct of dataProducts) {
+    const { categorySlug, ...product } = dataProduct;
+
+    await prisma.product.create({
+      data: {
+        ...product,
+        description: product.description ?? "No description",
+        category: { connect: { slug: categorySlug.toLowerCase() } },
+      },
     });
-    console.info(`Product: ${newProductResult.title}`);
   }
+}
+
+async function main() {
+  await seedCategories();
+  await seedProducts();
 }
 
 main()
