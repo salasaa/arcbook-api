a52fe35b6da69e278147a35a5a4f368007c795c7
salasaa <akhirudinsalasa.gc@gmail.com>
2025-10-01 15:02:44 +0700

    feat: add data categories

---
+++ prisma/data/categories.ts
@@ -0,0 +1,10 @@
+type Category = {
+  slug: string;
+  name: string;
+};
+
+export const dataCategories: Category[] = [
+  { slug: "comics", name: "Comics" },
+  { slug: "fiction", name: "Fiction" },
+  { slug: "non-fiction", name: "Non-Fiction" },
+];
